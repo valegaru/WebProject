3233a90b2147e8801664c4dec8b026a4d56aa324
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-04-07 21:55:20 -0500

    add_navbar readability and css

--- my-react-app/src/components/Navbar/Navbar.jsx
+++ my-react-app/src/components/Navbar/Navbar.jsx
@@ -4,31 +4,39 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import './Navbar.css'; 
 
 const Navbar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" elevation={0} sx={{ px: 4, py: 1 }} className="navbar">
+
+      <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+
+        <Box className="logo-container">
+          <Box className="logo-circle" />
+          <Typography className="nav-title">Postal Trip</Typography>
+        </Box>
+
+
+        <Stack direction="row" spacing={5} alignItems="center">
+          <Typography className="nav-link active">Home</Typography>
+          <Typography className="nav-link bold">Trips</Typography>
+          <Typography className="nav-link">Destination Matchmaker</Typography>
+        </Stack>
+
+        <Stack direction="row" spacing={2} alignItems="center">
+          <Avatar src="https://placehold.co/53x53" sx={{ width: 53, height: 53 }} />
+          <Button variant="contained" className="cta-button">
+            Create New Trip
+          </Button>
+        </Stack>
+
+      </Toolbar>
+
+    </AppBar>
   );
-}
+};
 
 export default Navbar;
