943457902e9308a99580ba42bc5a4596a7838e97
valegaru <valegaru2003@gmail.com>
2025-04-09 00:42:44 -0500

    feat footer landing

---
+++ my-react-app/src/components/CallToActionFooter/CallToActionFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './callToActionFooter.css';
+
+const CallToActionFooter = () => {
+	return (
+		<section className="cta-footer">
+			<div className="cta-overlay">
+				<h2>Sign up now and plan your next adventure effortlessly!</h2>
+				<button className="cta-btn">Start planing your trip <span>Now</span></button>
+			</div>
+		</section>
+	);
+};
+
+export default CallToActionFooter;
--- my-react-app/src/pages/landing/landing.jsx
+++ my-react-app/src/pages/landing/landing.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Banner from '../../components/Banner/banner';
 import TestimonialsList from '../../components/TestimonialList/TestimonialList';
+import CallToActionFooter from '../../components/CallToActionFooter/CallToActionFooter';
 const Landing = () => {
 	return (
 		<>
 			<Navbar></Navbar>
 			<Banner></Banner>
 			<TestimonialsList></TestimonialsList>
+			<CallToActionFooter></CallToActionFooter>
 			<p>LandingPage</p>
 		</>
 	);
