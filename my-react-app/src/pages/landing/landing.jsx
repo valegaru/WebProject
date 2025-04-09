0f00062adb2ec90c3d9bdbbcd75d6401330cb4b7
valegaru <valegaru2003@gmail.com>
2025-04-09 01:03:21 -0500

    feat featureCard

--- my-react-app/src/pages/landing/landing.jsx
+++ my-react-app/src/pages/landing/landing.jsx
@@ -3,6 +3,10 @@ import Navbar from '../../components/Navbar/Navbar';
 import Banner from '../../components/Banner/banner';
 import TestimonialsList from '../../components/TestimonialList/TestimonialList';
 import CallToActionFooter from '../../components/CallToActionFooter/CallToActionFooter';
+import FeatureCard from '../../components/FeatureCard/FeatureCard';
+import starburst from '../../assets/starburst.svg';
+import airplaneBadge from '../../assets/airplane-badge.svg';
+import planewindow from '../../assets/planewindow.png';
 const Landing = () => {
 	return (
 		<>
@@ -10,6 +14,22 @@ const Landing = () => {
 			<Banner></Banner>
 			<TestimonialsList></TestimonialsList>
 			<CallToActionFooter></CallToActionFooter>
+			<FeatureCard
+				title='Create Your Trip'
+				subtitle='Start by setting up your trip'
+				description='Give it a name, select dates, and invite your travel buddies. Whether it’s a weekend getaway or a long adventure, everything starts here!'
+				image={planewindow}
+				decorationRight={<img src={airplaneBadge} alt='Plane badge' style={{ width: 100 }} />}
+			/>
+
+			<FeatureCard
+				title='Collaborative Itineraries'
+				subtitle='Plan every detail of your trip, day by day, with your group.'
+				description='Effortlessly create shared travel schedules where everyone can add places, activities, and restaurants. Say goodbye to endless group chats and last-minute confusion!'
+				image='/assets/itinerary-map.jpg'
+				reverse
+				decorationLeft={<img src={starburst} alt='Plus icon' style={{ width: 80 }} />}
+			/>
 			<p>LandingPage</p>
 		</>
 	);
