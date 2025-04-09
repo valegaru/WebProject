b3c10dc22b048b335f7c8e7b1608aeb260595697
valegaru <valegaru2003@gmail.com>
2025-04-09 00:31:48 -0500

    feat testimonial.comments

--- my-react-app/src/pages/landing/landing.jsx
+++ my-react-app/src/pages/landing/landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Banner from '../../components/Banner/banner';
+import TestimonialsList from '../../components/TestimonialList/TestimonialList';
 const Landing = () => {
 	return (
 		<>
 			<Navbar></Navbar>
 			<Banner></Banner>
+			<TestimonialsList></TestimonialsList>
 			<p>LandingPage</p>
 		</>
 	);
