163f6274ffa61b5c53a62665ffdefd5e1e15e6db
valegaru <valegaru2003@gmail.com>
2025-04-09 00:03:04 -0500

    routes landing

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
- import Home from './pages/Home/Home'
- import Matchmaker from './pages/Matchmaker/Matchmaker'
- import ExpenseTracker from './pages/ExpenseTracker/ExpenseTracker'
- import TripPlanner from './pages/TripPlanner/TripPlanner'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Matchmaker from './pages/Matchmaker/Matchmaker';
+import ExpenseTracker from './pages/ExpenseTracker/ExpenseTracker';
+import TripPlanner from './pages/TripPlanner/TripPlanner';
+import Landing from './pages/landing/landing';
 
 const App = () => {
-   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/matchmaker' element={<Matchmaker/>}/>
-        <Route path='/expenseTracker' element={<ExpenseTracker/>}/>
-        <Route path='/tripPlanner' element={<TripPlanner/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/matchmaker' element={<Matchmaker />} />
+				<Route path='/expenseTracker' element={<ExpenseTracker />} />
+				<Route path='/tripPlanner' element={<TripPlanner />} />
+				<Route path='/landing' element={<Landing />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
--- my-react-app/src/pages/landing/landing.jsx
+++ my-react-app/src/pages/landing/landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Navbar from '../../components/Navbar/Navbar';
 
 const Landing = () => {
-  return (
-
-        <p>hola</p>
-  );
+	return (
+		<>
+			<Navbar></Navbar>
+			<p>LandingPage</p>
+		</>
+	);
 };
 
 export default Landing;
