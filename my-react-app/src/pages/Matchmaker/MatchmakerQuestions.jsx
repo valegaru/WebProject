26a3787f6c8ed093899f1289d001e7e23bb0ca05
valegaru <valegaru2003@gmail.com>
2025-04-10 04:02:41 -0500

    feat questionList

--- my-react-app/src/pages/Matchmaker/MatchmakerQuestions.jsx
+++ my-react-app/src/pages/Matchmaker/MatchmakerQuestions.jsx
@@ -1,12 +1,13 @@
 import Navbar from '../../components/Navbar/Navbar';
 import StepsProgressBar from '../../components/StepsProgressBar/StepsProgressBar';
-
+import QuestionList from '../../components/QuestionList/QuestionList';
 
 function MatchmakerQuestions() {
 	return (
 		<>
 			<Navbar></Navbar>
 			<StepsProgressBar currentStep={2} />
+			<QuestionList></QuestionList>
 		</>
 	);
 }
