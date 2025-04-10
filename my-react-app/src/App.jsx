7c2fb6dd1fc244c63bfbeedfd7afecb813e0391f
SofiRn <sofiaromeronar@gmail.com>
2025-04-09 20:45:58 -0500

    trip and responsive

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,29 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
-<<<<<<< HEAD
+
 import Trip from './pages/tripPlanner/TripPlanner';
-=======
+
 import Matchmaker from './pages/Matchmaker/Matchmaker';
 import ExpenseTracker from './pages/ExpenseTracker/ExpenseTracker';
 import './App.css';
 
 import Landing from './pages/landing/landing';
->>>>>>> camilo
+
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<Home />} />
-<<<<<<< HEAD
+D
 
-=======
+
 				<Route path='/matchmaker' element={<Matchmaker />} />
 				<Route path='/expenseTracker' element={<ExpenseTracker />} />
 				<Route path='/tripPlanner' element={<Trip />} />
 				<Route path='/landing' element={<Landing />} />
->>>>>>> camilo
+
 			</Routes>
 		</BrowserRouter>
 	);
