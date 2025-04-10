1272ec06b8055500a62c2cff509f34d2d7e483cf
valegaru <valegaru2003@gmail.com>
2025-04-10 02:38:16 -0500

    feat ProgressBar and Participant Card

--- my-react-app/src/pages/Matchmaker/MatchmakerQuestions.jsx
+++ my-react-app/src/pages/Matchmaker/MatchmakerQuestions.jsx
@@ -1,10 +1,16 @@
 import Navbar from '../../components/Navbar/Navbar';
-
+import StepsProgressBar from '../../components/StepsProgressBar/StepsProgressBar';
+import ParticipantCard from '../../components/ParticipantCard/ParticipantCard';
 function MatchmakerQuestions() {
 	return (
 		<>
 			<Navbar></Navbar>
 			<h1>MatchmakerQuestions</h1>
+			<StepsProgressBar currentStep={1} />
+			 <ParticipantCard
+      name="Sara V."
+      avatarUrl="https://randomuser.me/api/portraits/women/44.jpg"
+    />
 		</>
 	);
 }
