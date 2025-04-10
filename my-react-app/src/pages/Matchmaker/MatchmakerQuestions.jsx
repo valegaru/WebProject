9e93e1baf5170c2f80ad03ec55ee05fca3c4e687
valegaru <valegaru2003@gmail.com>
2025-04-10 02:02:27 -0500

    feat matchbanner

---
+++ my-react-app/src/pages/Matchmaker/MatchmakerQuestions.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../../components/Navbar/Navbar';
+
+function MatchmakerQuestions() {
+	return (
+		<>
+			<Navbar></Navbar>
+		</>
+	);
+}
+
+export default MatchmakerQuestions;
