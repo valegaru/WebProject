807e97708a1f2101e3b441fb33242e9bf55b3140
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-05-05 17:48:43 -0500

    fix import getFirestore

--- my-react-app/src/services/firebase.js
+++ my-react-app/src/services/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,4 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore(app); 
