eb4454c96929969a5f474f8a63b33c5d4c7b6ada
Camilo Game <tenorio2003@gmail.com>
2025-05-07 13:04:18 -0500

    add TripPlanner

---
+++ my-react-app/src/pages/TripPlanner/TripPlanner.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar/Navbar"
+
+function TripPlanner() {
+
+    return (
+      <>
+        <Navbar></Navbar>
+        <p>tri planer</p>
+      </>
+    )
+  }
+  
+  export default TripPlanner
+  
