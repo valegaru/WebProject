9ca256f5697d628eeb94ea9e228e0a10ba39ea30
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-08 10:00:48 -0500

    fix getAuth

--- my-react-app/src/services/firebase.js
+++ my-react-app/src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
