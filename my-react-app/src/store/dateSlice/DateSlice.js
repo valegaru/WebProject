42621e6e725cbe602afd6bff80814a3c19937701
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-10 16:55:47 -0500

    add dateSlice

---
+++ my-react-app/src/store/dateSlice/DateSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    selectedDate: null
+};
+
+export const DateSlice = createSlice({
+    name: 'selectedDate',
+    initialState,
+    reducers: {
+        setSelectedDate: (state, action) => {
+            state.selectedDate = action.payload;
+        }
+    }
+})
+
+export const {setSelectedDate} = DateSlice.actions;
+export default DateSlice.reducer
