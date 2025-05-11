856ef7d03e68a1567843d932668f33e76d236a21
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-11 16:42:07 -0500

    fix store reducer import

--- my-react-app/src/store/store.js
+++ my-react-app/src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { AuthReducer } from './auth/AuthSlice';
-import { DateReducer } from './dateSlice/DateSlice';
-import { CurrencyReducer } from './currencySlice/CurrencySlice';
+import  AuthReducer  from './auth/AuthSlice';
+import  DateReducer  from './dateSlice/DateSlice';
+import  CurrencyReducer  from './currencySlice/CurrencySlice';
 
 export const store = configureStore({
   reducer: {
--- my-react-app/src/utils/firebaseUtils.js
+++ my-react-app/src/utils/firebaseUtils.js
@@ -1,5 +1,6 @@
 import { db } from '../services/firebase';
 import { addDoc, collection } from 'firebase/firestore';
+import { doc, getDoc, getDocs, query, where } from 'firebase/firestore';
 
 export const addNewExpense= async ({ uidUser, name, price }) => {
     const docRef = await addDoc(collection(db, 'expense'), {
