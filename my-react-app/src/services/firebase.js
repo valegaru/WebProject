f857cbbaf966e5ee778d9a28f8a1a3214b64274c
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-11 16:46:18 -0500

    fix firebaseConfig

--- my-react-app/src/services/firebase.js
+++ my-react-app/src/services/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9EvmicJXAJpy6ylsg6d0IaD8R-zaPno0",
-  authDomain: "webprogramfinal.firebaseapp.com",
-  projectId: "webprogramfinal",
-  storageBucket: "webprogramfinal.firebasestorage.app",
-  messagingSenderId: "562432451959",
-  appId: "1:562432451959:web:a7e2082461ffc4672ded72",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
