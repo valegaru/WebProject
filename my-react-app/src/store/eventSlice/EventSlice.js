9ea694a354d5cc6195c3f9ffadd3dd118768bdf3
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-11 17:31:23 -0500

    add EventSLice

--- my-react-app/src/store/eventSlice/EventSlice.js
+++ my-react-app/src/store/eventSlice/EventSlice.js
@@ -1,3 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const eventSlice = createSlice({})
+const initialState = {
+    events: []
+};
+
+const eventSlice = createSlice({
+    name: 'events',
+    initialState,
+    reducers: {
+        addEvent: (state, action) => {
+            state.push(action.payload);
+    },
+        removeEvent: (state, action) => {
+            return state.filter(event => event.id !== action.payload);
+    },
+    updateEvent: (state, action) => {
+        const index = state.findIndex(event => event.id === action.payload.id);
+        if (index !== -1) {
+            state[index] = action.payload;
+        }
+    },
+},
+});
+
+export const { addEvent, removeEvent, updateEvent } = eventSlice.actions;
+export default eventSlice.reducer;
