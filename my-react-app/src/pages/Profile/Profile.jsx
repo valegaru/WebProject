635d076e4d94db8a6f99e4c33902960cf359dc37
valegaru <valegaru2003@gmail.com>
2025-05-12 03:13:38 -0500

    feature Profle basic

---
+++ my-react-app/src/pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../components/Navbar/Navbar';
+import Banner from '../../components/Banner/banner';
+function Profile() {
+	return (
+		<>
+			<Navbar></Navbar>
+			<p>Profile</p>
+		</>
+	);
+}
+
+export default Profile;
