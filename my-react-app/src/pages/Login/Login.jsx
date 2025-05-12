686c961b39e4d421a907cacba0c4601c8dba4727
valegaru <valegaru2003@gmail.com>
2025-05-12 01:22:58 -0500

    feature login con local storage

--- my-react-app/src/pages/Login/Login.jsx
+++ my-react-app/src/pages/Login/Login.jsx
@@ -1,37 +1,39 @@
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { setUser } from '../../store/auth/AuthSlice';
+import { setUserId } from '../../store/auth/AuthSlice'; 
 import { auth } from '../../services/firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState(null);
 
-  const handleLogin = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        dispatch(setUser(user.uid));
-        navigate('/');
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
-  };
+	const handleLogin = async () => {
+		try {
+			const userCredential = await signInWithEmailAndPassword(auth, email, password);
+			const user = userCredential.user;
 
-  return (
-    <>
-      <input type="text" onChange={(e) => setEmail(e.target.value)} placeholder="User email" />
-      <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button onClick={handleLogin}>Login</button>
-    </>
-  );
+			// Guardamos el ID del usuario en Redux y localStorage
+			dispatch(setUserId(user.uid));
+			navigate('/'); // Redirigir al home si todo salió bien
+		} catch (error) {
+			setError('Credenciales inválidas');
+			console.error(error.code, error.message);
+		}
+	};
+
+	return (
+		<div>
+			<input type='text' onChange={(e) => setEmail(e.target.value)} placeholder='User email' value={email} />
+			<input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' value={password} />
+			<button onClick={handleLogin}>Login</button>
+			{error && <p style={{ color: 'red' }}>{error}</p>} {/* Muestra el error */}
+		</div>
+	);
 };
 
 export default Login;
