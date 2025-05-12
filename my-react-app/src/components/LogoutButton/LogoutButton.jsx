c17d5ee1f6f937e822fe88ccaca96c8dff09a91f
valegaru <valegaru2003@gmail.com>
2025-05-12 02:17:25 -0500

    fix styles

--- my-react-app/src/components/LogoutButton/LogoutButton.jsx
+++ my-react-app/src/components/LogoutButton/LogoutButton.jsx
@@ -1,15 +1,16 @@
-import { useDispatch } from "react-redux";
-import { clearUserId } from "../../store/auth/AuthSlice";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { clearUserId } from '../../store/auth/AuthSlice';
+import { useNavigate } from 'react-router-dom';
+import './LogoutButton.css';
 
 export default function LogoutButton() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(clearUserId()); // Limpiar Redux y localStorage
-    navigate("/landing"); // Redirigir al landing
-  };
+	const handleLogout = () => {
+		dispatch(clearUserId()); // Limpiar Redux y localStorage
+		navigate('/landing'); // Redirigir al landing
+	};
 
-  return <button onClick={handleLogout}>Cerrar sesión</button>;
+	return <button id='logoutbutton' onClick={handleLogout}>Cerrar sesión</button>;
 }
