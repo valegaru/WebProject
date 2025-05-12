69e746e5d80ed7e933068ea971140691ac4656c7
valegaru <valegaru2003@gmail.com>
2025-05-12 02:06:48 -0500

    styles privRoute

--- my-react-app/src/components/PrivateRoute/PrivateRoute.jsx
+++ my-react-app/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import './PrivateRoute.css';
 
 const PrivateRoute = ({ children }) => {
 	const userId = useSelector((state) => state.auth.userId);
