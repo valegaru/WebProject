c3aa7bc8c901b4239980db406ddc8e96c8beea29
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-15 08:43:11 -0500

    profile pic click -> profile

--- my-react-app/src/components/ProfilePic/ProfilePic.jsx
+++ my-react-app/src/components/ProfilePic/ProfilePic.jsx
@@ -1,9 +1,11 @@
 import { Avatar } from "@mui/material";
 import "./ProfilePic.css";
 
-const ProfilePic = ({ name, imgUrl, isHighlighted }) => {
+const ProfilePic = ({ name, imgUrl, isHighlighted, onClick }) => {
   return (
-    <div className={`profile-pic ${isHighlighted ? "highlighted" : ""}`}>
+    <div
+      className={`profile-pic ${isHighlighted ? "highlighted" : ""}`} onClick={onClick} style={{ cursor: 'pointer' }} 
+    >
       <Avatar alt={name} src={imgUrl} className="profile-avatar" />
     </div>
   );
