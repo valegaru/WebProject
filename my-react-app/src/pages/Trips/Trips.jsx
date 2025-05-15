f0458375d5040d3c809e1da98b87da527c5f9feb
valegaru <valegaru2003@gmail.com>
2025-05-14 22:01:16 -0500

    features trips

---
+++ my-react-app/src/pages/Trips/Trips.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../components/Navbar/Navbar';
+import Banner from '../../components/Banner/banner';
+import LogoutButton from '../../components/LogoutButton/LogoutButton';
+function Trips() {
+	return (
+		<>
+			<Navbar></Navbar>
+			<p>Profile</p>
+      <LogoutButton></LogoutButton>
+		</>
+	);
+}
+
+export default Trips;
