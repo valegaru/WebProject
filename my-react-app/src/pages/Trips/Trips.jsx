1b974136a432e997c722c21aca92b6efd9f922bb
valegaru <valegaru2003@gmail.com>
2025-05-15 01:11:25 -0500

    feature banner trips

--- my-react-app/src/pages/Trips/Trips.jsx
+++ my-react-app/src/pages/Trips/Trips.jsx
@@ -1,11 +1,19 @@
 import Navbar from '../../components/Navbar/Navbar';
 import Banner from '../../components/Banner/banner';
-import LogoutButton from '../../components/LogoutButton/LogoutButton';
+import bannerimage from '../../assets/bannerimage.png';
+
 function Trips() {
 	return (
 		<>
 			<Navbar></Navbar>
-			<p>Trips</p>
+			<Banner
+				title='Browse through your trips'
+				subtitle='Click on a trip to view the details'
+				description='Are you ready to embark on your next adventure? Browse through your trips and get ready to create unforgettable memories with your friends and family.'
+				buttonText='Start planning your next trip'
+				backgroundImage={bannerimage}
+				buttonRoute='/tripcreation'
+			/>
 		</>
 	);
 }
