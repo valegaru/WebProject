5055ef5251bdefbf74bdf27ea3dce0070d4eef8b
valegaru <valegaru2003@gmail.com>
2025-05-14 23:20:02 -0500

    add email participant card

--- my-react-app/src/components/ParticipantCard/ParticipantCard.jsx
+++ my-react-app/src/components/ParticipantCard/ParticipantCard.jsx
@@ -1,13 +1,17 @@
+// ParticipantCard.js
 import React from 'react';
 import './ParticipantCard.css';
 
-const ParticipantCard = ({ name, avatarUrl }) => {
-  return (
-    <div className="participant-card">
-      <img className="avatar" src={avatarUrl} alt={name} />
-      <span className="name">{name}</span>
-    </div>
-  );
+const ParticipantCard = ({ name, avatarUrl, email }) => {
+	return (
+		<div className='participant-card'>
+			<img className='avatar' src={avatarUrl} alt={name} />
+			<div className='info'>
+				<span className='name'>{name}</span>
+				<span className='email'>{email}</span>
+			</div>
+		</div>
+	);
 };
 
 export default ParticipantCard;
