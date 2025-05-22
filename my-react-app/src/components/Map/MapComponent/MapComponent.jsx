6fdecbd51d9de72b0c8aac99b17ff45da8a163aa
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-05-22 13:32:16 -0500

    working on handleMapClick -> add Poi

--- my-react-app/src/components/Map/MapComponent/MapComponent.jsx
+++ my-react-app/src/components/Map/MapComponent/MapComponent.jsx
@@ -1,9 +1,40 @@
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, InfoWindow, Map } from "@vis.gl/react-google-maps";
 import PoiMarkers from "../PoiMarker/PoiMarkers";
+import { useState } from "react";
 
 const MapComponent = () => {
+
+    const [selectedLocation, setSelectedLocation] = useState();
+    const [showDialog, setShowDialog] = useState(false);
+    const [dialogLocation, setDialogLocation] = useState("");
+
+    const onAddLocation = () => {
+        console.log("Added")
+    }
+
+    const handleMapClick = (mapInfo) => {
+        if (mapInfo) {
+            const lat = mapInfo.location.lat
+            const lng = mapInfo.location.lng
+            setDialogLocation(lat,lng);
+            setSelectedLocation(lat,lng);
+            setShowDialog(true);
+        } else {
+            console.log("NO LOCATION SPECIFIED")
+        }
+    }
+
     return(
     <>
+    {showDialog && (
+
+    <InfoWindow position={dialogLocation}>
+        <button className="app-button" onClick={onAddLocation}>
+        Add this location
+        </button>
+    </InfoWindow>
+    )}
+
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
         <Map
             style={{width: '100vw', height: '50vh'}}
@@ -12,8 +43,9 @@ const MapComponent = () => {
             gestureHandling={'greedy'}
             disableDefaultUI={true}
             mapId={"3559db7569081dbf973e4ebf"}
+            onClick={(mapInfo)=>handleMapClick(mapInfo)}
         >
-            <PoiMarkers></PoiMarkers> 
+            <PoiMarkers locationInfo={selectedLocation}></PoiMarkers> 
         </Map>
     </APIProvider>
     </>
