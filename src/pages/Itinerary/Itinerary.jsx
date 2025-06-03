bc3b43f6d9964c118dbf4417dba9add46fb7b808
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-06-03 14:12:25 -0500

    add itinerary scaffholding

---
+++ src/pages/Itinerary/Itinerary.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar/Navbar"
+
+const itinerary = () => {
+    return(
+        <>
+            <Navbar></Navbar>
+        </>
+    )
+}
+
+export default itinerary
