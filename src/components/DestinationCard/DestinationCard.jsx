dec6da37b6103d6a5a9f1c6685a52ee0d58da0d8
valegaru <valegaru2003@gmail.com>
2025-06-07 22:54:46 -0500

    refractor

--- src/components/DestinationCard/DestinationCard.jsx
+++ src/components/DestinationCard/DestinationCard.jsx
@@ -1,20 +1,27 @@
-// src/components/DestinationCard/DestinationCard.jsx
 import React from 'react';
 import '../ParticipantCard/ParticipantCard.css'; // reuse styles
-import './DestinationCard.css';
 
 const DestinationCard = ({ name, flagUrl, onRemove }) => {
-	return (
-		<div className='participant-card destination-card'>
-			{flagUrl && <img className='avatar' src={flagUrl} alt={`${name} flag`} />}
-			<div className='info'>
-				<span className='name'>{name}</span>
-			</div>
-			<button className='remove-button' onClick={() => onRemove(name)}>
-				✖
-			</button>
-		</div>
-	);
+  return (
+    <div className="participant-card destination-card">
+      {flagUrl && (
+        <img
+          className="avatar"
+          src={flagUrl}
+          alt={`${name} flag`}
+        />
+      )}
+      <div className="info">
+        <span className="name">{name}</span>
+      </div>
+      <button
+        className="remove-button"
+        onClick={() => onRemove(name)}
+      >
+        ✖
+      </button>
+    </div>
+  );
 };
 
 export default DestinationCard;
