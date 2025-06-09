501017f160070dc8ff509b27d10fc304cbc131da
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-06-09 16:50:17 -0500

    update Home rendering

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
 		<AppBar elevation={0} sx={{ backgroundColor: '#f4e4c5', px: 4, py: 1 }} className='navbar'>
 			<Toolbar disableGutters sx={{ justifyContent: 'space-between', width: '100%' }}>
 				<Box className='logo-container'>
-					<img src='/src/assets/logo.png' className='logo-image' />
+					<img src='https://github.com/valegaru/WebProject/blob/main/src/assets/logo.png?raw=true' className='logo-image' />
 					{!isMobile && <p className='nav-title'>Postal Trip</p>}
 				</Box>
 
